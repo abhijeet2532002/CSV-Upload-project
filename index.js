1c05871e559b16922ea2f35d982b895c568bc335
abhij <abhijeetkumar2532002@gmail.com>
2023-09-26 19:13:39 +0530

    commit

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const PORT = process.env.PORT || 5005;
 const db = require('./config/mongoose')
-// import express layout
-// const expressLayout = require('express-ejs-layouts');
 const multer = require('multer');
 
 const app = express();
 
-// express Layout
-// app.use(expressLayout);
-// app.set('layout extractStyles', true);
-// app.set('layout extractScripts', true);
 app.use('/uploads',express.static(__dirname + '/uploads'));
 
 // Encode the url
